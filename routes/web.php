0cad8a11c2ba1f99051e82b0931dc5dbad237964
saviodev23 <saviodev21@gmail.com>
2024-04-16 20:51:52 -0300

    primeiro commit

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\SobreNosController;
+use app\Http\Controllers\TesteController;
+
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/sobrenos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/login', function(){ return 'login'; })->name('site.login');
+
+// /definição de agrupamento de rotas, add prefixo /app
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){ return 'Clientes'; })->name('app.clientes');
+    Route::get('/fornecedores', function(){ return 'Fornecedores'; })->name('app.fornecedores');
+    Route::get('/produtos', function(){ return 'Produtos'; })->name('app.produtos');
+});
+
+
+Route::get('/teste/{p1}/{p2}', [TesteController::class, 'teste'])->name('teste');
+
+
+
+// Route::redirect('/rota2', '/rota1');
+
+//Rota de fallback
+Route::fallback(function(){
+    echo 'a rota acessada não existe';
+});
